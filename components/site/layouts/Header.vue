0f5a798b6c0244360e3b713d65e84915948ad48e
dm1annem <300225@mail.ru>
2021-06-04 18:01:25 +0300

    first commit

---
+++ components/site/layouts/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <header class=" ">
+        <div class="grid grid-cols-12 gap-x-4 justify-between mt-3 mx-4 md:mx-10">
+            <div class="col-start-1 col-span-auto">
+                <NuxtLink  to="/">
+                    <img src="@/static/img/logo3.jpg">
+                </NuxtLink >
+            </div>
+            <div class="col-start-3 col-span-2 grid justify-self-start">
+                <span class=" text-gray-500 ">Проектирование и строительство загородных домов</span>
+            </div>
+            <div class="col-start-7">
+                <a href="https://ok.ru/karkascentr.ru" ><i class="fa fa-odnoklassniki mx-1 text-gray-500 text-xl" aria-hidden="true"></i></a>
+                <a href="https://www.youtube.com/channel/UCJoHMcmHq115v7XriMA5ZiQ?view_as=subscriber" ><i class="fa fa-youtube-square mx-1 text-gray-500 text-xl" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/karkascentr.ru/"><i class="fa fa-instagram mx-1 text-gray-500 text-xl" aria-hidden="true"></i></a>
+                <a href="https://www.facebook.com/karkascentr/" ><i class="fa fa-facebook mx-1 text-gray-500 text-xl" aria-hidden="true"></i></a>
+            </div>
+            <div class="col-end-13 col-span-2 grid justify-items-end  justify-self-end">
+                <p class="justify-self-end">
+                    <a href="https://wa.me/message/S2HVLZUQSNBBN1" class="hover:underline">
+                        WhatsApp <i class="fa fa-whatsapp text-xl ml-1 text-green-600" aria-hidden="true"></i> 
+                    </a>
+                </p> 
+                <p>
+                        <a href="tel:+79308200225">
+                            +7(4832)30-02-25 <i class="fa fa-phone"></i> 
+                        </a>	
+                </p>
+            </div>
+        </div>
+        <nav  class="flex justify-center w-full bg-yellow-100 ">
+            <span>menu</span>
+        </nav>
+    </header>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MyHeader/>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import MyHeader from '@/components/site/layouts/Header'
+export default {
+  components:{
+    MyHeader
+  }
+  
+}
+</script>
+
+<style>
+</style>
+
